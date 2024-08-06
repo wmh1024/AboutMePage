a228afafe54e5a843bf5ff11b72b6d00630bff9c
wmh <wmh031024@163.com>
2024-08-06 18:28:52 +0800

    fix: bugs

--- src/components/supporter.tsx
+++ src/components/supporter.tsx
@@ -1,32 +1,35 @@
 import {cn} from "@/lib/utils";
+import Link from "next/link";
 
 function SupporterList({supporters}: { supporters: SupporterType[] }) {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-6 mt-4">
       {supporters.map((supporter, idx) => (
-        <div
-          key={idx}
-          className={cn(
-            "relative group flex flex-col w-full h-full",
-            "bg-background-hover hover:bg-background-active focus:bg-background-active transition-all",
-            "rounded-lg shadow-md border cursor-pointer overflow-hidden"
-          )}
-        >
-          <div className="flex flex-col p-6 pb-4 grow">
-            <img
-              loading={"lazy"}
-              src={`/supporter/${supporter.avatar}`}
-              alt={supporter.name}
-              className="rounded-full w-20 h-20 self-center"
-            />
-            <h3 className="flex flex-row items-center text-lg font-bold mt-4">
-              {supporter.name}
-            </h3>
-            <p className="text-sm text-secondary mt-1.5 line-clamp-2 mb-auto">
-              {supporter.message}
-            </p>
+        <Link href="javascript:void(0);">
+          <div
+            key={idx}
+            className={cn(
+              "relative group flex flex-col w-full h-full",
+              "bg-background-hover hover:bg-background-active focus:bg-background-active transition-all",
+              "rounded-lg shadow-md border cursor-pointer overflow-hidden"
+            )}
+          >
+            <div className="flex flex-col p-6 pb-4 grow">
+              <img
+                loading={"lazy"}
+                src={`/supporter/${supporter.avatar}`}
+                alt={supporter.name}
+                className="rounded-full w-20 h-20 self-center"
+              />
+              <h3 className="flex flex-row items-center text-lg font-bold mt-4">
+                {supporter.name}
+              </h3>
+              <p className="text-sm text-secondary mt-1.5 line-clamp-2 mb-auto">
+                {supporter.message}
+              </p>
+            </div>
           </div>
-        </div>
+        </Link>
       ))}
     </div>
   );
